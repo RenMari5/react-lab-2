bce95b817e33efd07dddec2ec1ad7794a449cfc9
Ren <rpaczkowski@gmail.com>
2022-08-03 16:42:48 -0400

    Still working on errors...

--- react-lab-2/src/components/SocialPosts.tsx
+++ react-lab-2/src/components/SocialPosts.tsx
@@ -12,48 +12,44 @@ export default function SocialPost({}) {
   const [postList, setPostList] = useState<Post[]>([]);
 
   function handleOpenThought(e: React.ChangeEvent<HTMLInputElement>) {
-    if (!thought) {
-      return null;
-    }
+    const thought = false;
   }
-
-  // function handleSubmit(post: Post) {
-  //   setPostList([post, ...postList]);
-  // }
-
-  function handleDeletePost(index: number) {
-    return setPostList((prev) => {
-      const newList = prev.slice(0);
-      newList.splice(index, 1);
-      return newList;
-    });
-  }
-
-  function handleSubmitPost(index: number) {
-    return setPostList((postList) => {
-      const newList = postList.push();
-      return newList;
-    });
-  }
-
-  // function handleClosePost {
-
-  // }
-
-  return (
-    <div className="my-thoughts">
-      <header style={{ fontFamily: "Homemade Apple" }}>My Thoughts</header>
-      <button className="new-thought-button" onClick={() => handleOpenThought}>
-        New Thought
-      </button>
-      <PostForm
-        post={post}
-        onSubmit={handleSubmitPost}
-        // onClose={handleClosePost}
-      />
-      {postList.map((post, index) => (
-        <PostInList post={post} postIndex={index} onDelete={handleDeletePost} />
-      ))}
-    </div>
-  );
 }
+
+// function handleSubmit(post: Post) {
+//   setPostList([post, ...postList]);
+// }
+
+function handleDeletePost(index: number) {
+  return setPostList((prev) => {
+    const newList = prev.slice(0);
+    newList.splice(index, 1);
+    return newList;
+  });
+}
+
+function handleSubmitPost(postList: Post[]) {
+  return setPostList((post) => {
+    const newList = postList.push(post);
+    return newList;
+  });
+}
+
+function handleCloseForm();
+
+return (
+  <div className="my-thoughts">
+    <header style={{ fontFamily: "Homemade Apple" }}>My Thoughts</header>
+    <button className="new-thought-button" onClick={() => handleOpenThought}>
+      New Thought
+    </button>
+    <PostForm
+      post={post}
+      onSubmit={handleSubmitPost}
+      onClick={handleCloseForm}
+    />
+    {postList.map((post, index) => (
+      <PostInList post={post} postIndex={index} onDelete={handleDeletePost} />
+    ))}
+  </div>
+);
